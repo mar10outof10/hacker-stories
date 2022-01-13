4f9c40e0b6cfd261855050f7d282f982da5da3af
Martin Halas <marhaldev@gmail.com>
2022-01-13 12:47:45 -0500

    Finished chapter 'React JSX'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-const title = 'React';
-
+const welcome = {
+  greeting: 'Heya',
+  title: 'React'
+};
+function getTitle(title) {
+  return title;
+}
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <h1>
+        Hello {getTitle('React')}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
