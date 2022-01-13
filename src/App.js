b7cfddca36cb3cc7a76e108a2c5853e7bc38af10
Martin Halas <marhaldev@gmail.com>
2022-01-13 16:20:57 -0500

    Finished another react component chapter

--- src/App.js
+++ src/App.js
@@ -25,29 +25,41 @@ function App() {
   return (
     <div className="App">
       <h1>My Hacker Stories</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      
+      <Search />
 
       <hr />
-      
-      <ul style={{color: 'purple', display: 'flex', flexDirection:'column', width:'400px', margin:'0 auto'}}>
-        {list.map((item) => {
-          return (
-            <li key={item.objectID} style={{paddingBottom: '12px', listStyle: 'inside square', display:'grid', gridTemplateColumns:'1fr 3fr 1fr 1fr', columnGap:'18px'}}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>Author: {item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-      {/* commentsss */}
+
+      <List />
     </div>
   );
 }
 
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  )
+}
+
+function List() {
+  return (
+    <ul style={{color: 'purple', display: 'flex', flexDirection:'column', width:'400px', margin:'0 auto'}}>
+      {list.map((item) => {
+        return(
+          <li key={item.objectID} style={{paddingBottom: '12px', listStyle: 'inside square', display:'grid', gridTemplateColumns:'1fr 3fr 1fr 1fr', columnGap:'18px'}}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
 export default App;
