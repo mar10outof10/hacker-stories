5af5498e38a1fcb04f5a06c2899abe4cb38659bc
Martin Halas <marhaldev@gmail.com>
2022-01-14 13:12:30 -0500

    completed callback function section

--- src/App.js
+++ src/App.js
@@ -22,12 +22,16 @@ const App = () => {
       objectID: 1,
     }
   ];
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  }
   return (
 
     <div className="App">
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch} />
 
       <hr />
 
@@ -36,11 +40,13 @@ const App = () => {
   )
 };
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+
+    props.onSearch(event);
   }
   return(
     <div>
