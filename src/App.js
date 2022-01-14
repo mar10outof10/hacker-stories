0c6ccb63aef6423a5642d7e8d8da90adc39d9b74
Martin Halas <marhaldev@gmail.com>
2022-01-13 20:24:07 -0500

    Completed chapter on props

--- src/App.js
+++ src/App.js
@@ -1,65 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {    
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+import React, { useState } from 'react';
 
 
-function App() {
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ];
   return (
+
     <div className="App">
       <h1>My Hacker Stories</h1>
-      
+
       <Search />
 
       <hr />
 
-      <List />
+      <List list={stories}/>
     </div>
-  );
-}
+  )
+};
 
-function Search() {
-  return (
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  }
+  return(
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
     </div>
   )
-}
+};
 
-function List() {
-  return (
-    <ul style={{color: 'purple', display: 'flex', flexDirection:'column', width:'400px', margin:'0 auto'}}>
-      {list.map((item) => {
-        return(
-          <li key={item.objectID} style={{paddingBottom: '12px', listStyle: 'inside square', display:'grid', gridTemplateColumns:'1fr 3fr 1fr 1fr', columnGap:'18px'}}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
+const List = (props) => (
+  <ul style={{ color: 'purple', display: 'flex', flexDirection: 'column', width: '400px', margin: '0 auto' }}>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+)
+
+const Item = (props) => (
+      <li style={{ paddingBottom: '12px', listStyle: 'inside square', display: 'grid', gridTemplateColumns: '1fr 3fr 1fr 1fr', columnGap: '18px' }}>
+        <span>
+          <a href={props.item.url}>{props.item.title}</a>
+        </span>
+        <span>{props.item.author}</span>
+        <span>{props.item.num_comments}</span>
+        <span>{props.item.points}</span>
+      </li>
+)
 export default App;
