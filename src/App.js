3a076070cc11c5444386144d8776d5f9cf94f1db
Martin Halas <marhaldev@gmail.com>
2022-01-21 15:57:26 -0500

    Refactored search component

--- src/App.js
+++ src/App.js
@@ -50,7 +50,13 @@ const App = () => {
     <div className="App">
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search: "
+        value={searchTerm}
+        type="text"
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
@@ -59,11 +65,16 @@ const App = () => {
   )
 };
 
-const Search = ({search, onSearch}) => (
+const InputWithLabel = ({id, label, value, type="text", onInputChange}) => (
   <>
     <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" value={search} onChange={onSearch}/>
+      <label htmlFor={label}>{label}</label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
     </div>
   </>
 )
