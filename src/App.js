0544213e03f5b50d94265353cdf68c1d70f2ac47
Martin Halas <marhaldev@gmail.com>
2022-01-26 17:02:06 -0500

    Reformatted code declaration order

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
 
-const useSemiPersistentState = (key, initialState) => {
-  const [value, setValue] = useState(
-    localStorage.getItem(key) ?? initialState
-  )
-
-  useEffect(() => {
-    localStorage.setItem(key, value)
-  }, [value, key])
-
-  return [value, setValue];
-}
-
 const initialStories = [
   {
     title: 'React',
@@ -32,53 +20,17 @@ const initialStories = [
   }
 ];
 
-const App = () => {
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = useState(
+    localStorage.getItem(key) ?? initialState
+  )  
 
-  const [searchTerm, setSearchTerm] = useSemiPersistentState(
-    'search',
-    'React'
-  );
+  useEffect(() => {
+    localStorage.setItem(key, value)
+  }, [value, key])  
 
-  const [stories, setStories] = useState(initialStories);
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-  
-  const handleRemovedStory = (item) => {
-    const newStories = stories.filter(
-      story => item.objectID !== story.objectID
-    );
-    console.log(item, {...stories});
-
-    setStories(newStories);
-  }
-
-  const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
-
-  return (
-
-    <div className="App">
-      <h1>My Hacker Stories</h1>
-
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearch}
-      >
-        <strong>Search: </strong>
-      </InputWithLabel>
-
-      <hr />
-
-      <List
-        list={searchedStories}
-        onRemoveItem={handleRemovedStory}
-      />
-    </div>
-  )
-};
+  return [value, setValue];
+}  
 
 const InputWithLabel = ({id, value, type="text", isFocused, onInputChange, children}) => {
   const inputRef = useRef();
@@ -134,4 +86,55 @@ const Item = ({item, onRemoveItem}) => {
     </li>
   )
 }
+
+
+
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
+
+  const [stories, setStories] = useState(initialStories);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  
+  const handleRemovedStory = (item) => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+    console.log(item, {...stories});
+
+    setStories(newStories);
+  }
+
+  const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  return (
+
+    <div className="App">
+      <h1>My Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search: </strong>
+      </InputWithLabel>
+
+      <hr />
+
+      <List
+        list={searchedStories}
+        onRemoveItem={handleRemovedStory}
+      />
+    </div>
+  )
+};
+
 export default App;
