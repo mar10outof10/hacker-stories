024b5f3fca53d463e0bda71b206d5ebdb6821f39
Martin Halas <marhaldev@gmail.com>
2022-02-01 23:50:59 -0500

    Added api stressing hackernews fetching to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import './App.css';
 import React, { useState, useEffect, useRef, useReducer } from 'react';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -52,15 +35,6 @@ const storiesReducer = (state, action) => {
       throw new Error();
   }
 };
-
-const getAsyncStories = () => (
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: {stories: initialStories }}),
-      2000
-    )
-  )
-);
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = useState(
@@ -147,19 +121,22 @@ const App = () => {
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
     
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}${searchTerm}`)
+    .then((response) => response.json())
     .then(result => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
+        payload: result.hits,
       });
     })
     .catch(() => 
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
     );
-  }, [])
+  }, [searchTerm])
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
