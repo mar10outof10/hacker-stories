242faf9b38e18b4a8858c3506c66f8a336043864
Martin Halas <marhaldev@gmail.com>
2022-02-02 00:32:59 -0500

    Replaced fetch with axios, installed axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState, useEffect, useRef, useReducer, useCallback } from 'react';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -123,17 +124,17 @@ const App = () => {
   const handleFetchStories = useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
     
-    fetch(url)
-    .then((response) => response.json())
-    .then(result => {
-      dispatchStories({
-        type: 'STORIES_FETCH_SUCCESS',
-        payload: result.hits,
-      });
-    })
-    .catch(() => 
-      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-    );
+    axios
+      .get(url)
+      .then(result => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.data.hits,
+        });
+      })
+      .catch(() => 
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+      );
   }, [url]);
 
   useEffect(() => {
